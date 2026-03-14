be4e07105393a9cf15d2692462b572cee572c3b4
Ashish suthar <sutharashish119@gmail.com>
2026-03-14 14:20:12 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -180,3 +180,26 @@ this.classList.add("active");
 });
 
 });
+
+// STOCK TABS
+document.querySelectorAll(".tab-btn").forEach(btn => {
+
+btn.addEventListener("click", function(){
+
+document.querySelectorAll(".tab-btn").forEach(b=>{
+b.classList.remove("active");
+});
+
+document.querySelectorAll(".tab-content").forEach(c=>{
+c.classList.remove("active");
+});
+
+this.classList.add("active");
+
+let tab = this.getAttribute("data-tab");
+
+document.getElementById(tab).classList.add("active");
+
+});
+
+});
